b9fb669b8d8404fc445d5fa49564af0c80be62db
viiiraj07 <avisec07@gmail.com>
2021-11-11 12:52:01 +0530

    slide wise text and image with dominant picture algo

--- src/main/java/com/exportapp/ppt/services/Helper.java
+++ src/main/java/com/exportapp/ppt/services/Helper.java
@@ -4,17 +4,13 @@ import com.exportapp.ppt.entity.Collection;
 import com.exportapp.ppt.jpa.CollectionRepo;
 import org.apache.poi.sl.extractor.SlideShowExtractor;
 import org.apache.poi.sl.usermodel.PictureData;
-import org.apache.poi.sl.usermodel.Shape;
-import org.apache.poi.sl.usermodel.Slide;
-import org.apache.poi.sl.usermodel.SlideShow;
-import org.apache.poi.ss.usermodel.Picture;
 import org.apache.poi.xslf.usermodel.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.*;
-import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -23,7 +19,7 @@ public class Helper {
     @Autowired
     private CollectionRepo collectionRepo;
 
-    public String fetchText(MultipartFile ppt){
+    public String fetchText(MultipartFile ppt) {
 
         XMLSlideShow slideshow = null;
 
@@ -33,72 +29,87 @@ public class Helper {
             e.printStackTrace();
         }
 
-        SlideShowExtractor<XSLFShape,XSLFTextParagraph> slideShowExtractor
-                = new SlideShowExtractor<XSLFShape,XSLFTextParagraph>(slideshow);
+        SlideShowExtractor<XSLFShape, XSLFTextParagraph> slideShowExtractor
+                = new SlideShowExtractor<XSLFShape, XSLFTextParagraph>(slideshow);
 
-        for (XSLFSlide slide: slideshow.getSlides()){
-            for (XSLFShape shape:slide.getShapes()){
 
+        for (XSLFSlide slide : slideshow.getSlides()) {
+
+            List<XSLFPictureShape> xslfPictureShapeList = new ArrayList<>();
+            for (XSLFShape shape : slide.getShapes()) {
                 //for images
                 if (shape instanceof XSLFPictureShape) {
                     XSLFPictureShape xslfPictureData = (XSLFPictureShape) shape;
-                    XSLFPictureData pict = xslfPictureData.getPictureData();
-                    byte[] data = pict.getData();
-                    PictureData.PictureType type = pict.getType();
-                    String ext;
-                    switch (type) {
-                        case JPEG:
-                            ext = ".jpg";
-                            break;
-                        case PNG:
-                            ext = ".png";
-                            break;
-                        case WMF:
-                            ext = ".wmf";
-                            break;
-                        case EMF:
-                            ext = ".emf";
-                            break;
-                        case PICT:
-                            ext = ".pict";
-                            break;
-                        default:
-                            continue;
-                    }
-                    FileOutputStream out;
-                    try {
-                        File folder = new File(String.format("%d", slide.getSlideNumber()));
-                        folder.mkdir();
-                        File pic = new File(String.format("%d/%d%s", slide.getSlideNumber(), shape.getShapeId(), ext));
-                        out = new FileOutputStream(pic);
-                        out.write(data);
-                        out.close();
-                    } catch (IOException e) {
-                        e.getLocalizedMessage();
-                    }
+                    xslfPictureShapeList.add(xslfPictureData);
                 }
-
                 //for text
                 if (shape instanceof XSLFTextShape) {
-                    XSLFTextShape textShape = (XSLFTextShape)shape;
+                    XSLFTextShape textShape = (XSLFTextShape) shape;
                     String text = textShape.getText();
-                    if(!text.contains(":")) break;
+                    if (!text.contains(":")) break;
                     String[] array = text.split(":");
 
                     Collection collection = new Collection();
-                    try{
+                    try {
                         collection.setStyleNo(array[1].trim());
                         collection.setDescription(array[2].trim());
                         collection.setComp(array[3].trim());
                         collection.setSize(array[4].trim());
                         collection.setWeight(array[5].trim());
-                    }catch (ArrayIndexOutOfBoundsException e){
+                    } catch (ArrayIndexOutOfBoundsException e) {
                         e.getLocalizedMessage();
-                    }finally {
+                    } finally {
                         //collectionRepo.save(collection);
                     }
                 }
             }
+
+            //storing dominant image
+            if (xslfPictureShapeList.size() == 0) continue;
+            XSLFPictureShape maxImage = xslfPictureShapeList.get(0);
+            double area = Double.MIN_VALUE;
+            for (XSLFPictureShape currentPic : xslfPictureShapeList) {
+                double currentArea = currentPic.getAnchor().getWidth() * currentPic.getAnchor().getHeight();
+                if (currentArea > area) {
+                    area = currentArea;
+                    maxImage = currentPic;
+                }
+            }
+
+            byte[] data = maxImage.getPictureData().getData();
+            PictureData.PictureType type = maxImage.getPictureData().getType();
+            String ext;
+            switch (type) {
+                case JPEG:
+                    ext = ".jpg";
+                    break;
+                case PNG:
+                    ext = ".png";
+                    break;
+                case WMF:
+                    ext = ".wmf";
+                    break;
+                case EMF:
+                    ext = ".emf";
+                    break;
+                case PICT:
+                    ext = ".pict";
+                    break;
+                default:
+                    continue;
+            }
+            FileOutputStream out;
+            try {
+                File folder = new File(String.format("%d", slide.getSlideNumber()));
+                folder.mkdir();
+                File pic = new File(String.format("%d/%s%s", slide.getSlideNumber(), "pic", ext));
+                out = new FileOutputStream(pic);
+                out.write(data);
+                out.close();
+            } catch (IOException e) {
+                e.getLocalizedMessage();
+            }
+
         }
 
         //extractImage(slideshow);
@@ -107,32 +118,43 @@ public class Helper {
         return slideShowExtractor.getText();
     }
 
-    private void extractImage(XMLSlideShow ppt){
+    private void extractImage(XMLSlideShow ppt) {
         List<XSLFPictureData> pdata = ppt.getPictureData();
-        for (int i = 0; i < pdata.size(); i++){
+        for (int i = 0; i < pdata.size(); i++) {
             XSLFPictureData pict = pdata.get(i);
 
             byte[] data = pict.getData();
 
             PictureData.PictureType type = pict.getType();
             String ext;
-            switch (type){
-                case JPEG: ext=".jpg"; break;
-                case PNG: ext=".png"; break;
-                case WMF: ext=".wmf"; break;
-                case EMF: ext=".emf"; break;
-                case PICT: ext=".pict"; break;
-                default: continue;
+            switch (type) {
+                case JPEG:
+                    ext = ".jpg";
+                    break;
+                case PNG:
+                    ext = ".png";
+                    break;
+                case WMF:
+                    ext = ".wmf";
+                    break;
+                case EMF:
+                    ext = ".emf";
+                    break;
+                case PICT:
+                    ext = ".pict";
+                    break;
+                default:
+                    continue;
             }
             FileOutputStream out;
             try {
-                out = new FileOutputStream("pict_"+ (i+1) + ext);
+                out = new FileOutputStream("pict_" + (i + 1) + ext);
                 out.write(data);
                 out.close();
             } catch (IOException e) {
                 e.getLocalizedMessage();
             }
-            //TODO: Cloud storage and URI in Database
+            //TODO: Cloud storage and URI in Database0
         }
     }
 }
