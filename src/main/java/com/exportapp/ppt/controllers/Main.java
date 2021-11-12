d2d3ad937e7fd6aa664a8af2254c54eb3b10088b
viiiraj07 <avisec07@gmail.com>
2021-11-12 10:26:23 +0530

    code breakdown

--- src/main/java/com/exportapp/ppt/controllers/Main.java
+++ src/main/java/com/exportapp/ppt/controllers/Main.java
@@ -14,8 +14,28 @@ public class Main {
     @Autowired
     Helper helper;
 
-    @PostMapping("/fetchtext")
-    public String fetchText(@RequestParam("file") MultipartFile file){
-        return helper.fetchText(file);
+    @PostMapping("/1")
+    public void extractAllImages(@RequestParam("file") MultipartFile file){
+        helper.extractAllImages(file);
+    }
+
+    @PostMapping("/2")
+    public void extractAllImagesSlideWise(@RequestParam("file") MultipartFile file){
+        helper.extractAllImagesSlideWise(file);
+    }
+
+    @PostMapping("/3")
+    public void extractMostDominantImageFromSlide(@RequestParam("file") MultipartFile file){
+        helper.extractMostDominantImageFromSlide(file);
+    }
+
+    @PostMapping("/4")
+    public String extractAllTextFromSlideshow(@RequestParam("file") MultipartFile file){
+        return helper.extractAllTextFromSlideshow(file);
+    }
+
+    @PostMapping("/5")
+    public void extractTextSlideWise(@RequestParam("file") MultipartFile file){
+        helper.extractTextSlideWise(file);
     }
 }
--- src/main/java/com/exportapp/ppt/services/Helper.java
+++ src/main/java/com/exportapp/ppt/services/Helper.java
@@ -19,6 +19,212 @@ public class Helper {
     @Autowired
     private CollectionRepo collectionRepo;
 
+    public void extractAllImages(MultipartFile ppt) {
+
+        XMLSlideShow slideshow = null;
+
+        try {
+            slideshow = new XMLSlideShow(ppt.getInputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        int picId = 0;
+
+        for (XSLFPictureData pictureDatum : slideshow.getPictureData()) {
+            byte[] picData = pictureDatum.getData();
+            PictureData.PictureType pictureType = pictureDatum.getType();
+            String ext;
+            switch (pictureType) {
+                case JPEG:
+                    ext = ".jpg";
+                    break;
+                case PNG:
+                    ext = ".png";
+                    break;
+                case WMF:
+                    ext = ".wmf";
+                    break;
+                case EMF:
+                    ext = ".emf";
+                    break;
+                case PICT:
+                    ext = ".pict";
+                    break;
+                default:
+                    continue;
+            }
+
+            FileOutputStream out;
+            try {
+                out = new FileOutputStream((picId++) + ext);
+                out.write(picData);
+                out.close();
+            } catch (IOException e) {
+                e.getLocalizedMessage();
+            }
+        }
+    }
+
+    public void extractAllImagesSlideWise(MultipartFile ppt) {
+
+        XMLSlideShow slideshow = null;
+
+        try {
+            slideshow = new XMLSlideShow(ppt.getInputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        for (XSLFSlide slide : slideshow.getSlides()) {
+            for (XSLFShape shape : slide.getShapes()) {
+                if (shape instanceof XSLFPictureShape) {
+                    XSLFPictureShape xslfPictureData = (XSLFPictureShape) shape;
+                    byte[] data = xslfPictureData.getPictureData().getData();
+                    PictureData.PictureType type = xslfPictureData.getPictureData().getType();
+                    String ext;
+                    switch (type) {
+                        case JPEG:
+                            ext = ".jpg";
+                            break;
+                        case PNG:
+                            ext = ".png";
+                            break;
+                        case WMF:
+                            ext = ".wmf";
+                            break;
+                        case EMF:
+                            ext = ".emf";
+                            break;
+                        case PICT:
+                            ext = ".pict";
+                            break;
+                        default:
+                            continue;
+                    }
+                    FileOutputStream out;
+                    try {
+                        File folder = new File(String.format("%d", slide.getSlideNumber()));
+                        folder.mkdir();
+                        File pic = new File(String.format("%d/%d%s", slide.getSlideNumber(), shape.getShapeId(), ext));
+                        out = new FileOutputStream(pic);
+                        out.write(data);
+                        out.close();
+                    } catch (IOException e) {
+                        e.getLocalizedMessage();
+                    }
+                }
+            }
+        }
+    }
+
+    public void extractMostDominantImageFromSlide(MultipartFile ppt) {
+        XMLSlideShow slideshow = null;
+
+        try {
+            slideshow = new XMLSlideShow(ppt.getInputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        for (XSLFSlide slide : slideshow.getSlides()) {
+            List<XSLFPictureShape> xslfPictureShapeList = new ArrayList<>();
+            for (XSLFShape shape : slide.getShapes()) {
+                if (shape instanceof XSLFPictureShape) {
+                    XSLFPictureShape xslfPictureData = (XSLFPictureShape) shape;
+                    xslfPictureShapeList.add(xslfPictureData);
+                }
+            }
+
+            if (xslfPictureShapeList.size() == 0) continue;
+
+            XSLFPictureShape maxImage = xslfPictureShapeList.get(0);
+            double area = Double.MIN_VALUE;
+
+            for (XSLFPictureShape currentPic : xslfPictureShapeList) {
+                double currentArea = currentPic.getAnchor().getWidth() * currentPic.getAnchor().getHeight();
+                if (currentArea > area) {
+                    area = currentArea;
+                    maxImage = currentPic;
+                }
+            }
+
+            byte[] data = maxImage.getPictureData().getData();
+            PictureData.PictureType type = maxImage.getPictureData().getType();
+            String ext;
+            switch (type) {
+                case JPEG:
+                    ext = ".jpg";
+                    break;
+                case PNG:
+                    ext = ".png";
+                    break;
+                case WMF:
+                    ext = ".wmf";
+                    break;
+                case EMF:
+                    ext = ".emf";
+                    break;
+                case PICT:
+                    ext = ".pict";
+                    break;
+                default:
+                    continue;
+            }
+
+            FileOutputStream out;
+
+            try {
+                File folder = new File(String.format("%d", slide.getSlideNumber()));
+                folder.mkdir();
+                File pic = new File(String.format("%d/%s%s", slide.getSlideNumber(), "pic", ext));
+                out = new FileOutputStream(pic);
+                out.write(data);
+                out.close();
+            } catch (IOException e) {
+                e.getLocalizedMessage();
+            }
+        }
+    }
+
+    public String extractAllTextFromSlideshow(MultipartFile ppt) {
+        XMLSlideShow slideshow = null;
+
+        try {
+            slideshow = new XMLSlideShow(ppt.getInputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        SlideShowExtractor<XSLFShape, XSLFTextParagraph> slideShowExtractor
+                = new SlideShowExtractor<XSLFShape, XSLFTextParagraph>(slideshow);
+
+        return slideShowExtractor.getText();
+    }
+
+    public void extractTextSlideWise(MultipartFile ppt) {
+        XMLSlideShow slideshow = null;
+
+        try {
+            slideshow = new XMLSlideShow(ppt.getInputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        for (XSLFSlide slide : slideshow.getSlides()) {
+            for (XSLFShape shape : slide.getShapes()) {
+                if (shape instanceof XSLFTextShape) {
+                    System.out.println("Slide number: " + slide.getSlideNumber());
+                    System.out.println();
+                    System.out.println(((XSLFTextShape) shape).getText());
+                    System.out.println();
+                }
+            }
+        }
+    }
+}
+
+/*
     public String fetchText(MultipartFile ppt) {
 
         XMLSlideShow slideshow = null;
@@ -158,3 +364,4 @@ public class Helper {
         }
     }
 }
+*/
